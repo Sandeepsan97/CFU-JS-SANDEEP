483eb8ec4bebef5f713961469a9b3c5c8debf8da
Sandeepsan97 <sandeep909600@gmail.com>
2023-03-09 19:41:59 +0530

    comiit

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -10,28 +10,12 @@ function charLength1() {
   
 
 }
-
 document.getElementById('length').addEventListener('change', charLength1);
-
-
-
-
-
-
-
 
 const generateBTn=document.getElementById("btn");
 generateBTn.addEventListener("click",e=>{
-    e.preventDefault
-    generator()
-})
 
-
-
-
-
-function generator(){
-
+  const length=  document.getElementById('length').addEventListener('change', charLength1);
 
 
     let result=document.getElementById("password__result")
@@ -42,77 +26,116 @@ function generator(){
     const numbersCasebtn=document.getElementById("numbers").checked;
     const symbolCasebtn=document.getElementById("symbols").checked;
 
+
     let passwordLength = charLength;
-const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerCase="abcdefghijklmnopqrstuvwxyz"
-const numbers="1234567890";
-const symbols="!@#$%^&*()"
-let password = '';
 
+    let check="";
 
-if(upperCasebtn==true){
-    for(let i=0;i<passwordLength;i++){
-        const id_generator=Math.floor(Math.random() * uppercaseLetters.length);
-        password+=uppercaseLetters[id_generator]
-      
-result.value=password
-    
+    if(upperCasebtn==true){
+        check+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    }
+    if(lowerCasebtn==true){
+        check+="abcdefghijklmnopqrstuvwxyz"
+
+    }
+    if(numbersCasebtn==true){
+        check+="1234567890"
+
+    }
+    if(symbolCasebtn==true){
+        check+="!@#$%^&*()"
+
     }
 
 
-    
 
-}
-if(lowerCasebtn==true){
+    let store="";
+
     for(let i=0;i<passwordLength;i++){
-        const id_generator=Math.floor(Math.random() * lowerCase.length);
-        password+=lowerCase[id_generator]
+                const id_generator=Math.floor(Math.random() * check.length);
+                store+=check[id_generator]
+              
+        result.value=store
+            
+            }
     
-result.value=password
-    }
-    
-
-}
-if(numbersCasebtn==true){
-    for(let i=0;i<passwordLength;i++){
-        const id_generator=Math.floor(Math.random() * numbers.length);
-        password+=numbers[id_generator]
-    
-result.value=password
-    }
     
 
-}
-
-if(symbolCasebtn==true){
-    for(let i=0;i<passwordLength;i++){
-        const id_generator=Math.floor(Math.random() * symbols.length);
-        password+=symbols[id_generator]
-    
-result.value=password
-    }
+})
 
 
-}
-// if(upperCasebtn==true || lowerCasebtn==true || numbersCasebtn==true || symbolCasebtn==true){
+
+
+
+// function generator(){
+
+
+
+//     let result=document.getElementById("password__result")
+//     // console.log(result);
+//     const upperCasebtn=document.getElementById("uppercase").checked;
+
+//     const lowerCasebtn=document.getElementById("lowercase").checked;
+//     const numbersCasebtn=document.getElementById("numbers").checked;
+//     const symbolCasebtn=document.getElementById("symbols").checked;
+
+//     let passwordLength = charLength;
+
+// const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// const lowerCase="abcdefghijklmnopqrstuvwxyz"
+// const numbers="1234567890";
+// const symbols="!@#$%^&*()"
+// let password = '';
+
+
+// if(upperCasebtn==true){
 //     for(let i=0;i<passwordLength;i++){
-//         const id_generator=Math.floor(Math.random() * symbols.length *numbers.length *lowerCase.length*uppercaseLetters.length);
+//         const id_generator=Math.floor(Math.random() * uppercaseLetters.length);
+//         password+=uppercaseLetters[id_generator]
+      
+// result.value=password
+    
+//     }
+
+
+    
+
+// }
+// if(lowerCasebtn==true){
+//     for(let i=0;i<passwordLength;i++){
+//         const id_generator=Math.floor(Math.random() * lowerCase.length);
+//         password+=lowerCase[id_generator]
+    
+// result.value=password
+//     }
+    
+
+// }
+// if(numbersCasebtn==true){
+//     for(let i=0;i<passwordLength;i++){
+//         const id_generator=Math.floor(Math.random() * numbers.length);
+//         password+=numbers[id_generator]
+    
+// result.value=password
+//     }
+    
+
+// }
+
+// if(symbolCasebtn==true){
+//     for(let i=0;i<passwordLength;i++){
+//         const id_generator=Math.floor(Math.random() * symbols.length);
 //         password+=symbols[id_generator]
     
 // result.value=password
 //     }
+
+
 // }
 
-
-
-
-
-
-
-
-
-console.log(password);
+// console.log(password);
     
-}
+// }
 
 
